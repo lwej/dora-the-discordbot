5c2db76bd0a3e16cc6de718085c79e4e0a63255e
Leyla Wejdell <leylawbascunan@gmail.com>
2021-05-20 21:54:30 +0100

    need autoscroll something

--- pi_display.py
+++ pi_display.py
@@ -7,7 +7,7 @@ from RPLCD import cursor
 
 def send_msg(str):
     lcd = CharLCD(pin_rs=12, pin_rw=17, pin_e=16, pins_data=[6, 13, 19, 26], numbering_mode=GPIO.BCM, cols=16, rows=2,
-                  auto_linebreaks=False)    
+                  auto_linebreaks=True)    
     lcd.clear()
     lcd.cursor_pos = 0, 0
     lcd.write_string(str[:32])
